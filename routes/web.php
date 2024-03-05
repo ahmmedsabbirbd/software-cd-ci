86f0fee3e88ff4c5c10e74a47b8b077f118dd309
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2024-03-05 22:05:11 +0600

    work complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserDetailsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Routes for User CRUD operations
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/create', [UserController::class, 'create']);
+Route::post('/users', [UserController::class, 'store']);
+Route::get('/users/{user}', [UserController::class, 'show']);
+Route::get('/users/{user}/edit', [UserController::class, 'edit']);
+Route::put('/users/{user}', [UserController::class, 'update']);
+Route::delete('/users/{user}', [UserController::class, 'destroy']);
+
+// Routes for UserDetails CRUD operations
+Route::get('/user-details', [UserDetailsController::class, 'index']);
+Route::get('/user-details/create', [UserDetailsController::class, 'create']);
+Route::post('/user-details', [UserDetailsController::class, 'store']);
+Route::get('/user-details/{user_details}', [UserDetailsController::class, 'show']);
+Route::get('/user-details/{user_details}/edit', [UserDetailsController::class, 'edit']);
+Route::put('/user-details/{user_details}', [UserDetailsController::class, 'update']);
+Route::delete('/user-details/{user_details}', [UserDetailsController::class, 'destroy']);
