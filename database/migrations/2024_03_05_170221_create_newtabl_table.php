cd23e9424f0341f051fce4da97cd7fb4b7c235aa
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2024-03-05 23:28:58 +0600

    finaaly done

--- database/migrations/2024_03_05_170221_create_newtabl_table.php
+++ database/migrations/2024_03_05_170221_create_newtabl_table.php
@@ -44,5 +44,25 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('age');
+            $table->string('phone');
+        });
+        DB::statement('UPDATE users INNER JOIN user_details ON users.id = user_details.user_id SET users.age = user_details.age, users.phone = user_details.phone');
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn('age');
+            $table->dropColumn('phone');
+        });
+
+
+
+        Schema::table('user_details', function (Blueprint $table) {
+            $table->dropColumn('father_name');
+        });
+        DB::statement('UPDATE user_details INNER JOIN users ON user_details.user_id = users.id SET user_details.email = users.email, user_details.status = users.status');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('email');
+            $table->dropColumn('status');
+        });
     }
 };
