207292ba4fb1183128cf1e1ccbb2ff614350cb85
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2024-03-05 23:17:05 +0600

    adsf

--- database/migrations/2024_03_05_170221_create_newtabl_table.php
+++ database/migrations/2024_03_05_170221_create_newtabl_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) { 
+        Schema::table('users', function (Blueprint $table) {
             $table->string('email')->nullable()->after('phone');
             $table->string('status')->nullable()->after('email');
             $table->dropColumn(['age', 'phone']);
